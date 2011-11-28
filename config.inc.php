4c3090730d4d0107ca4d3579489a012c87b4870c
Nicholas Crown <nick@thecrowns.org>
2011-11-27 22:43:57 -0600

    Add database singleton

---
+++ config.inc.php
@@ -0,0 +1,12 @@
+<?php
+//database server
+define('DB_SERVER', "localhost");
+
+//database login name
+define('DB_USER', "root");
+//database login password
+define('DB_PASS', "password");
+
+//database name
+define('DB_DATABASE', "database");
+?>
