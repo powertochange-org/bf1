7da34ac30ece43254f80666c7909651014551e76
Nicholas Crown <nick@thecrowns.org>
2012-02-20 22:06:27 -0600

    Removing unused image files

--- modules/module.php
+++ modules/module.php
@@ -8,7 +8,7 @@
 ?>
 <div id="module<?php echo str_replace('.', '', $module['Number']); ?>" class="module">
     <div id="background_img">
-        <img src="<?php echo '../'.$module['Banner']; ?>"</img>
+        <img src="<?php echo '../'.$module['FrontImg']; ?>"</img>
     </div>
     <div id="continue">
         <a href="?s=<?php echo $module['FirstSection']; ?>" class="ui-state-default ui-corner-all shadow-medium">Continue<span class="ui-icon ui-icon-circle-triangle-e"></span></a>
