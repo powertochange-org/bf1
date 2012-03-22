b84d18f71402ca96eb87bb3250fa099436fa6f75
Nicholas Crown <nick@thecrowns.org>
2012-03-21 21:06:43 -0500

    Issue #10 & #22: Initial commit of the new welcome page and home page.  The modules will now only appear on the home page, which requires a user to login to the site.

---
+++ about.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Cru Doctrine
+ * About
+ * Campus Crusade for Christ
+ */
+
+//header
+require('header.php');
+?>
+<link rel="stylesheet" type="text/css" media="screen" href="/css/home.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/css/welcome.css" />
+<div id="content">
+    <div id="about" class="about">
+        <div id="title">
+            <div id="name">ABOUT CRUDOCTRINE</div>
+        </div>
+        <div id="banner">
+            <img src="/upload/images/Home%20Page%20Long.jpg"</img>
+        </div>
+        <div id="description">
+            <p>Welcome to CruDoctrine.  If you are new to CruDoctrine, or if this is your first time to the new and improved format, we have included some helpful information below to help you get acquainted with the site.  
+            Using the Cru Statement of Faith as a guide, CruDoctrine is an interactive tool to give you an introduction to systematic theology while seeking to engage your heart to worship Christ and engage in ministry.  
+            The key to CruDoctrine is that it is designed to be experienced with assistance from an advisor, or coach.</p>
+            <p>When you register as a new user, you will be asked which region you are from (either one of the 10 Cru regions in the U.S., a particular country, student discipleship region, or church discipleship region) and the name of your coach.  
+            If your coach has already logged in and set up their profile, you should be able to select them as your coach.  
+            If not, you can provide their email address and CruDoctrine will send them a note with instructions on how to register as a coach.</p>
+            <p>Once you log in, you’ll notice that each module is laced with video clips, audio clips, and questions along the way.  
+            And, you’ll notice that some of the questions are flagged for follow-up with your coach.  
+            Lastly, if you come to a question that you want to come back to again later or to ask your coach about, you can click either the “flag for personal follow up” or “flag for coach follow up”.</p>
+            <p>You’ll also notice that there are little note tabs that can be expanded on each page.  
+            As you take notes, you will always be able to see them and any flagged questions under the “My Work” tab.  
+            And, it is here that you can see your progress and when you can anticipate being finished with the course.
+            You’ll also notice the “community” tab.  Not only are you experiencing this material with your coach, but there are also thousands of users around the globe with whom you will also have the opportunity to interact.  
+            In the very near future, we will be adding additional features that will help you stay connected with those users.  
+            For instance, you will be given the option to “blog” some of your responses to questions related to the material that will be visible to everyone on the community page.  
+            Also, at the end of every module, you will be prompted to summarize the module in 140 characters or less.  
+            These responses will also be posted on the CruDoctrine live twitter feed on the community page.</p>
+            <p>Finally, you will want to regularly come back to the “resources” page.  
+            It is here that we will be adding additional content for further study.  
+            Whether it’s a video or audio clip or article, this page will be a never-ending resource page for you and for your ministry. 
+            Thanks for using CruDoctrine and feel free to email me with any questions or suggestions @ jeff.lark@uscm.org</p>
+        </div>
+    </div>
+</div>
+<?php
+//footer
+require('footer.php');
+?>
---
+++ welcome.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Cru Doctrine
+ * Welcome
+ * Campus Crusade for Christ
+ */
+
+?>
+<link rel="stylesheet" type="text/css" media="screen" href="/css/welcome.css" />
+<div id="welcome" class="welcome">
+    <div id="background_img">
+        <img src="/upload/images/Home%20Page.jpg"</img>
+    </div>
+    <!--div id="continue">
+        <a href="#register" class="ui-state-default ui-corner-all shadow-medium">Continue<span class="ui-icon ui-icon-circle-triangle-e"></span></a>
+    </div-->
+    <div id="title">
+        <div id="number">Welcome to</div>
+        <div id="name">CRUDoctrine</div>
+    </div>
+    <div id="description">
+        Using the <a href="http://www.ccci.org/about-us/statement-of-faith/index.htm">Cru Statement of Faith</a> as a guide, CruDoctrine is an interactive tool to give you an introduction to systematic theology while seeking to engage your heart to worship Christ and engage in ministry.
+        For more information on how to get started, see our <a href="about.php">about</a> page.
+    </div>
+</div>
