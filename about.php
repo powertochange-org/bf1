64233e1ab20e01c6f772b4f67945833a1e766bc4
Nicholas Crown <nick@thecrowns.org>
2012-03-21 21:43:08 -0500

    Issue #10 & #22: Updated the about page to remove copy associated with unreleased features.

--- about.php
+++ about.php
@@ -30,16 +30,8 @@ require('header.php');
             Lastly, if you come to a question that you want to come back to again later or to ask your coach about, you can click either the “flag for personal follow up” or “flag for coach follow up”.</p>
             <p>You’ll also notice that there are little note tabs that can be expanded on each page.  
             As you take notes, you will always be able to see them and any flagged questions under the “My Work” tab.  
-            And, it is here that you can see your progress and when you can anticipate being finished with the course.
-            You’ll also notice the “community” tab.  Not only are you experiencing this material with your coach, but there are also thousands of users around the globe with whom you will also have the opportunity to interact.  
-            In the very near future, we will be adding additional features that will help you stay connected with those users.  
-            For instance, you will be given the option to “blog” some of your responses to questions related to the material that will be visible to everyone on the community page.  
-            Also, at the end of every module, you will be prompted to summarize the module in 140 characters or less.  
-            These responses will also be posted on the CruDoctrine live twitter feed on the community page.</p>
-            <p>Finally, you will want to regularly come back to the “resources” page.  
-            It is here that we will be adding additional content for further study.  
-            Whether it’s a video or audio clip or article, this page will be a never-ending resource page for you and for your ministry. 
-            Thanks for using CruDoctrine and feel free to email me with any questions or suggestions @ jeff.lark@uscm.org</p>
+            And, it is here that you can see your progress and when you can anticipate being finished with the course.</p>
+            <p>Thanks for using CruDoctrine and feel free to email me with any questions or suggestions @ jeff.lark@uscm.org</p>
         </div>
     </div>
 </div>
