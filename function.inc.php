e7472127b40028e2a2e748ebb2ea662d9d0fb736
Nicholas Crown <nick@thecrowns.org>
2012-05-06 22:13:42 -0500

    Refactoring.

---
+++ function.inc.php
@@ -0,0 +1,12 @@
+<?php
+//functions
+
+function in_array_r($needle, $haystack, $strict = true) {
+    foreach ($haystack as $item) {
+        if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
+            return true;
+        }
+    }
+    return false;
+}
+?>
