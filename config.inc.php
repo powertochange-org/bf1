74e5607ae203d048a5b8b7718ffef57ec1ba7466
Nicholas Crown <nick@thecrowns.org>
2013-03-28 06:50:29 -0500

    Issue #68: Initial commit of Password Reset feature.

--- config.inc.php
+++ config.inc.php
@@ -45,7 +45,9 @@ define("REPORTS", "reports");
 define("DISCIPLES_REPORT", "disciples_report");
 define("PROGRESS_REPORT", "progress_report");
 define("SMTP_SERVER", "smtp.gmail.com");
-define("ADMIN_EMAIL_USERNAME", "administrator@crudoctrine.org");
+define("SMTP_PORT", 465);
+define("SMTP_SSL", "sslv3");
+define("ADMIN_EMAIL_USERNAME", "admin@crudoctrine.org");
 define("ADMIN_EMAIL_FULLNAME", "CruDoctrine Administrator");
-define("ADMIN_EMAIL_PASSWORD", "password");
+define("ADMIN_EMAIL_PASSWORD", "test");
 ?>
