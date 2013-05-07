e3ac6ee0e77ff84eae82916a060b817123091386
Nicholas Crown <nick@thecrowns.org>
2013-05-06 23:12:02 -0700

    Issue #130: Fixed a bug in the remove page function.

--- admin/remove.php
+++ admin/remove.php
@@ -8,118 +8,118 @@
 require_once("../config.inc.php"); 
 require_once("../Database.singleton.php");
 
-function deleteModule($id){
+function deleteModule($id) {
+  global $db;
 
-    global $db;
+  //delete attached sections
+  $sql = "SELECT * FROM section WHERE ModuleId = ".$id;
+  //execute query
+  $sections = $db->fetch_array($sql);
+  foreach($sections as $section) {
+      deleteSection($section['ID']);
+  }
 
-    //delete attached sections
-    $sql = "SELECT * FROM section WHERE ModuleId = ".$id;
-    //execute query
-    $sections = $db->fetch_array($sql);
-    foreach($sections as $section){
-        deleteSection($section['ID']);
-    }
-    
+  //delete progress
+  $sql = "DELETE FROM progress WHERE ID = ".$id." AND Type = '".MODULE."'";
+  $db->query($sql);
 
-    //delete progress
-    $sql = "DELETE FROM progress WHERE ID = ".$id." AND Type = '".MODULE."'";
-    $db->query($sql);
-
-    //delete module
-    $sql = "DELETE FROM module WHERE ID= ".$id;
-    //execute query
-    $db->query($sql);    
+  //delete module
+  $sql = "DELETE FROM module WHERE ID= ".$id;
+  //execute query
+  $db->query($sql);
 }
 
-function deleteSection($id){
-
-    global $db;
+function deleteSection($id) {
+  global $db;
+  
+  //delete attached pages
+  $sql = "SELECT * FROM page WHERE SectionId = ".$id;
+  $pages = $db->fetch_array($sql);
+  foreach($pages as $page) {
+      deletePage($page['ID']);
+  }
     
-    //delete attached pages
-    $sql = "SELECT * FROM page WHERE SectionId = ".$id;
-    $pages = $db->fetch_array($sql);
-    foreach($pages as $page){
-        deletePage($page['ID']);
-    }
-    
-    //delete section
-    $sql = "DELETE FROM section WHERE ID= ".$id;
-    //execute query
-    $db->query($sql);
+  //delete section
+  $sql = "DELETE FROM section WHERE ID= ".$id;
+  //execute query
+  $db->query($sql);
 }
 
-function deletePage($id){
+function deletePage($id) {
+  global $db;
+  
+  //delete attached elements
+  $sql = "SELECT * FROM element WHERE PageId = ".$id;
+  $elements = $db->fetch_array($sql);
 
-    global $db;
-    
-    //delete attached elements
-    $sql = "SELECT * FROM element WHERE PageId = ".$id;
-    $elements = $db->fetch_array($sql);
-
-    foreach($elements as $element) {
-        if ($element['Type'] == 'input'){
-             $sql = "DELETE FROM response WHERE InputId = ".$element['ElementId'];
-             //execute query
-             $db->query($sql);
-         }
-
-        $sql = "DELETE FROM ".$element['Type']." WHERE ID = ".$element['ElementId'];
-        //execute query
-        $db->query($sql);
-
-        $sql = "DELETE FROM element WHERE ElementId = ".$element['ElementId'];
-        //execute query
-        $db->query($sql);
+  foreach($elements as $element) {
+    if ($element['Type'] == 'input') {
+      $sql = "DELETE FROM response WHERE InputId = ".$element['ElementId'];
+      //execute query
+      $db->query($sql);
     }
 
-    //delete progress
-    $sql = "DELETE FROM progress WHERE ID = ".$id." AND Type = '".PAGE."'";
+    //delete notes
+    $sql = "DELETE FROM note WHERE ElementId = ".$element['ElementId'];
     $db->query($sql);
 
-    //delete page
-    $sql = "DELETE FROM page WHERE ID = ".$id;
+    //delete the element type
+    $sql = "DELETE FROM ".$element['Type']." WHERE ID = ".$element['ElementId'];
+    //execute query
     $db->query($sql);
+
+    //delete the element
+    $sql = "DELETE FROM element WHERE ElementId = ".$element['ElementId'];
+    //execute query
+    $db->query($sql);
+  }
+
+  //delete progress
+  $sql = "DELETE FROM progress WHERE ID = ".$id." AND Type = '".PAGE."'";
+  $db->query($sql);
+
+  //delete page
+  $sql = "DELETE FROM page WHERE ID = ".$id;
+  $db->query($sql);
 }
 
 try {
-    //get values
-    $ajax       = isset($_POST['ajax'])     ? true : false;
+  //get values
+  $ajax       = isset($_POST['ajax'])     ? true : false;
 
-    $type       = isset($_POST['type'])     ? $_POST['type'] : '';
-    $id         = isset($_POST['id'])       ? $_POST['id'] : '';
+  $type       = isset($_POST['type'])     ? $_POST['type'] : '';
+  $id         = isset($_POST['id'])       ? $_POST['id'] : '';
 
-    $errors     = isset($_POST['errors'])   ? $_POST['errors'] : '';
+  $errors     = isset($_POST['errors'])   ? $_POST['errors'] : '';
 
-    //initialize the database object
-    $db = Database::obtain(DB_SERVER, DB_USER, DB_PASS, DB_DATABASE); 
-    $db->connect();
+  //initialize the database object
+  $db = Database::obtain(DB_SERVER, DB_USER, DB_PASS, DB_DATABASE); 
+  $db->connect();
 
-    switch($type){
+  switch($type) {
+    case MODULE:
+      deleteModule($id);
+      break;
 
-        case 'module':
-            deleteModule($id);
-            break;
+    case SECTION:
+      deleteSection($id);
+      break;
 
-        case 'section':
-            deleteSection($id);
-            break;
+    case PAGE:
+      deletePage($id);
+      break;
+  }
 
-        case 'page':
-            deletePage($id);
-            break;
+  $db->close();
 
-    }
+  //if ajax, return success
+  if ($ajax) {
+    echo 1;
+  }
 
-    $db->close();
-
-    //if ajax, return success
-    if ($ajax) {
-
-        echo 1;
-    }
-
-} catch (PDOException $e) {
-    echo $e->getMessage();
-    exit();
+} 
+catch (PDOException $e) {
+  echo $e->getMessage();
+  exit();
 }
 ?>
